f32a337f22ebb1d4fe8039f3afa01835b9d06bd5
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2024-10-04 21:10:22 +0200

    first commit

---
+++ src/main/java/ch/guggisberg/stefan/microservice/mqttexample/MqttExampleApplication.java
@@ -0,0 +1,13 @@
+package ch.guggisberg.stefan.microservice.mqttexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/guggisberg/stefan/microservice/mqttexample/MqttExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.guggisberg.stefan.microservice.mqttexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
